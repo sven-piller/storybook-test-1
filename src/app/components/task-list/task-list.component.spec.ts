e512df2c7cf6dd2a15e8801865bb3ce61038cebe
Sven Piller <sven.piller@gmail.de>
2020-08-01 21:59:50 +0200

    Tutorial: Composite component - Tests
    https://www.learnstorybook.com/intro-to-storybook/angular/en/composite-component/

--- src/app/components/task-list/task-list.component.spec.ts
+++ src/app/components/task-list/task-list.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render } from '@testing-library/angular';
 import { TaskListComponent } from './task-list.component';
+import { TaskComponent } from '../task/task.component';
+import { withPinnedTasksData } from './task-list.stories';
 
 describe('TaskListComponent', () => {
-  let component: TaskListComponent;
-  let fixture: ComponentFixture<TaskListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TaskListComponent]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  test('should create', async () => {
+    const tree = await render(TaskListComponent);
+    const component = tree.fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test('renders pinned tasks at the start of the list', async () => {
+    const mockedActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        tasks: withPinnedTasksData,
+        loading: false,
+        onPinTask: {
+          emit: mockedActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockedActions,
+        } as any,
+      },
+    });
+    const component = tree.fixture.componentInstance;
+    expect(component.tasksInOrder[0].id).toBe('6');
   });
 });
