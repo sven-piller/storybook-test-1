8b1574691ed7645428079e81ed94cc7c7bea7543
Sven Piller <sven.piller@gmail.de>
2020-08-01 16:31:13 +0200

    Typescript preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes','@storybook/addon-backgrounds/register'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-notes',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/preset-typescript'
+  ],
 };
