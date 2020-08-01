82d9c89ac81fdbfd126c72afacd0d70ba7723929
Sven Piller <sven.piller@gmail.de>
2020-08-01 20:57:22 +0200

    Tutorial: TaskListComponent
    und TaskComponent in neuen Ordner verschoben

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
