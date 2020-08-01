a4d8cbd6cc98594f5e974b3df2a8b03298b28dae
Sven Piller <sven.piller@gmail.de>
2020-08-01 18:01:53 +0200

    Anpassungen für JEST

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  test('should create the app', async (() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'angular-storybook'`, () => {
+  test(`should have as title 'angular-storybook'`, async (() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-storybook');
-  });
+  }));
 
-  it('should render title in a h1 tag', () => {
+  test('should render title in a h1 tag', async (() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-storybook!');
-  });
+  }));
 });
